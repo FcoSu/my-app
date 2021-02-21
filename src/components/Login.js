47bb958ccc4a66f3fc6f67cec1a7e585163fcacf
Francisco Javier Seco Urrutia <panchoseur@gmail.com>
2021-02-21 16:54:47 -0300

    login catch errors

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,67 +1,78 @@
 /* eslint-disable no-undef */
 import React, { useState } from "react";
-import {auth} from '../firebaseconfig'
-
-
+import { auth } from "../firebaseconfig";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [msgError, setMsgError] = useState(null);
 
-
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [msgError, setMsgError] = useState(null)
-
-    const RegistrarUsuario = (e) => {
-        e.preventDefault()
-        auth.createUserWithEmailAndPassword(email, password)
-            .then( r => alert('Usuario registrado'))
-            .catch(e => {
-                if(e.code == 'auth/invalid-email'){
-                    setMsgError('Formato incorrecto')
-                }
-                if(e.code == 'auth/weak-password'){
-                    setMsgError('La password debe tener 6 caracteres o mas')
-                }
-            })
+  const RegistrarUsuario = (e) => {
+    e.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((r) => {
+        alert("Usuario registrado");
+        setMsgError(null);
+      })
+      .catch((e) => {
+        if (e.code == "auth/invalid-email") {
+          setMsgError("Formato incorrecto");
         }
-    
+        if (e.code == "auth/weak-password") {
+          setMsgError("La password debe tener 6 caracteres o mas");
+        }
+      });
+  };
 
-
-
+  const LoginUsuario = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((r) => {
+        console.log(r);
+        setMsgError(null);
+      })
+      .catch((err) => {
+        if (err.code == "auth/wrong-password") {
+          setMsgError("Contraseña incorrecta");
+        }
+        if (err.code == "auth/user-not-found") {
+          setMsgError("Correo invalido");
+        }
+      });
+  };
 
   return (
     <div className="row mt-5">
       <div className="col"></div>
       <div className="col">
         <form onSubmit={RegistrarUsuario} className="form-group">
-            <input
-            onChange={(e) => {setEmail(e.target.value)}}
-                className="form-control"
-                type="text" 
-                placeholder="Introduce el Email" />
-            <input
-            onChange={(e) => {setPassword(e.target.value)}}
-                className="form-control mt-4"
-                type="password" 
-                placeholder="Introduce la password" />
-            <input
-                className="btn btn-dark btn-block mt-4"
-                value="Registrar Usuario"
-                type="submit"/>
+          <input
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            className="form-control"
+            type="text"
+            placeholder="Introduce el Email"
+          />
+          <input
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            className="form-control mt-4"
+            type="password"
+            placeholder="Introduce la password"
+          />
+          <input
+            className="btn btn-dark btn-block mt-4"
+            value="Registrar Usuario"
+            type="submit"
+          />
         </form>
-           {
-               msgError != null ? (
-                   <div>
-                        {msgError}
-                   </div>
-               ):(
-                   <span>
-
-                   </span>
-               )
-           } 
-
-
+        <button onClick={LoginUsuario} className="btn btn-success btn-block">
+          Iniciar sesion
+        </button>
+        {msgError != null ? <div>{msgError}</div> : <span></span>}
       </div>
       <div className="col"></div>
     </div>
