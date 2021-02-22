1a1c65aee5ce63225ee15e732606a8e44097df7d
Francisco Javier Seco Urrutia <panchoseur@gmail.com>
2021-02-21 23:10:47 -0300

    borrar tarea implement

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,78 +1,118 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import { store } from "../firebaseconfig";
 
 const Admin = () => {
+  const [tarea, setTarea] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [todos, setTodos] = useState("");
+  const [error, setError] = useState("");
 
-const [tarea, setTarea] = useState('')
-const [descripcion, setDescripcion] = useState('')
-const [id, setId] = useState('')
-const[error, setError] = useState('')
+  useEffect(() => {
+    const getTodos = async () => {
+      const { docs } = await store.collection("todos").get();
+      const todosarray = docs.map((item) => ({ id: item.id, ...item.data() }));
+      setTodos(todosarray);
+    };
+    getTodos();
+  }, []);
 
-const setTareas= async (e) => {
-    e.preventDefault()
-    if(!tarea.trim()){
-        setError('La tarea no puede estar vacía')
+  const setTareas = async (e) => {
+    e.preventDefault();
+    if (!tarea.trim()) {
+      setError("La tarea no puede estar vacía");
     }
-    if(!descripcion.trim()){
-        setError('La descripción no puede estar vacía')
+    if (!descripcion.trim()) {
+      setError("La descripción no puede estar vacía");
     }
-    if(!descripcion.trim() && !tarea.trim()){
-        setError('Los campos no pueden estar vacíos')
+    if (!descripcion.trim() && !tarea.trim()) {
+      setError("Los campos no pueden estar vacíos");
     }
     const todo = {
-        tarea: tarea,
-        descripcion:descripcion
+      tarea: tarea,
+      descripcion: descripcion,
+    };
+    try {
+      const data = await store.collection("todos").add(todo);
+      const { docs } = await store.collection("todos").get();
+      const todosarray = docs.map((item) => ({ id: item.id, ...item.data() }));
+      setTodos(todosarray);
+    } catch (e) {
+      console.log(e);
     }
-    try{
-        
-        const data = await store.collection('todos').add(todo)
-        console.log('tarea añadida')
-        
+    setTarea("");
+    setDescripcion("");
+  };
+
+  const borrarTarea = async (id) => {
+    try {
+      await store.collection("todos").doc(id).delete();
+      const { docs } = await store.collection("todos").get();
+      const todosarray = docs.map((item) => ({ id: item.id, ...item.data() }));
+      setTodos(todosarray);
+    } catch (e) {
+      console.log(e);
     }
-    catch(e){
-        console.log(e)}
-    setTarea('')
-    setDescripcion('')
-}
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <h2>To Do's</h2>
-                    <form onSubmit={setTareas}>
-                        <input 
-                        value={tarea}
-                        onChange={(e) => {setTarea(e.target.value)}}
-                        className="form-control"
-                        placeholder="Introduce la tarea"
-                        type="text"
-                        />
-                        <input
-                        value={descripcion}
-                        onChange={(e) => {setDescripcion(e.target.value)}}
-                        className="form-control mt-3"
-                        placeholder="Introduce la descripción"
-                        type="text"
-                        />
-                        <input type="submit" className="btn btn-dark btn-block mt-3"/>
-                    </form>
-                    {
-                        error ? 
-                        (<div>
-                            <p>{error}</p>
-                        </div>)
-                        :
-                        (<span></span>)
-                    }
-
-                </div>
-                <div className="col">
-                    <h2>Lista de To Do's</h2>
-                </div>
-
+  };
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h2>To Do's</h2>
+          <form onSubmit={setTareas}>
+            <input
+              value={tarea}
+              onChange={(e) => {
+                setTarea(e.target.value);
+              }}
+              className="form-control"
+              placeholder="Introduce la tarea"
+              type="text"
+            />
+            <input
+              value={descripcion}
+              onChange={(e) => {
+                setDescripcion(e.target.value);
+              }}
+              className="form-control mt-3"
+              placeholder="Introduce la descripción"
+              type="text"
+            />
+            <input type="submit" className="btn btn-dark btn-block mt-3" />
+          </form>
+          {error ? (
+            <div>
+              <p>{error}</p>
             </div>
+          ) : (
+            <span></span>
+          )}
         </div>
-    )
-}
+        <div className="col">
+          <h2>Lista de To Do's</h2>
 
-export default Admin
+          <ul className="list-group">
+            {todos.length !== 0 ? (
+              todos.map((item) => (
+                <li
+                  key={item.id}
+                  className="list-group-item list-group-item-dark"
+                >
+                  {item.tarea}
+                  <button 
+                  className="btn btn-danger float-right"
+                  onClick={(id)=>{borrarTarea(item.id)}}
+                  >X</button>
+                  <ul>{item.descripcion}</ul>
+                </li>
+              ))
+            ) : (
+              <span>La lista está vacia</span>
+            )}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Admin;
